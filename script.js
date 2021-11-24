511ee7d4e392a3abb7913da8bd243573628bbe97
Martin <martinembon@hotmail.com>
2021-11-24 16:03:02 -0300

    upload

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const accordion = document.getElementsByClassName("contentBx")
 
+/*
 window.addEventListener('load', function(){
   accordion[0].classList.toggle("active")
 });
-
 const removeActiveClass = () => {  
   for (const activeElement of accordion) {
     activeElement.classList.remove('active');    
   }
 };
+*/
+
+
 
 for (i=0; i<accordion.length;i++){    
     accordion[i].addEventListener("click",function(e){      
