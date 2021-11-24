c4dbb96d8aeeab7d937f543d8740f404b4276f7e
Martincho <martinembon@hotmail.com>
2021-11-24 18:49:39 -0300

    detalhe produto pimenta

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
 const accordion = document.getElementsByClassName("contentBx")
 
-/*
-window.addEventListener('load', function(){
-  accordion[0].classList.toggle("active")
-});
-const removeActiveClass = () => {  
-  for (const activeElement of accordion) {
-    activeElement.classList.remove('active');    
-  }
-};
-*/
-
-
-
 for (i=0; i<accordion.length;i++){    
     accordion[i].addEventListener("click",function(e){      
-      //removeActiveClass()
       this.classList.toggle("active")               
     })                                            
 }
